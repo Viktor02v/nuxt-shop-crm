7bffea72913aefec5ee54d9a28fb22021fc04ea3
Viktor Petruk <damnnn261@gmail.com>
2024-12-19 19:42:04 +0200

    add (Table) from shadcn

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,7 @@ const { data: womanShoes, isPending: isPendingWoman, isError: isErrorWoman } = u
 		<div v-if="manShoes" class="mt-5">
 			<div class="w-full items-center justify-between flex gap-5">
 				<div class="w-1/2 border">
-					<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-5">
-						<div v-for="shoes in manShoes" :key="shoes.$id" class="">
-							<div class="h-[100px] hover:scale-110 transition-all duration-500 rounded p-5 border">
-								<NuxtImg :src="shoes.foto_url" class="w-full  h-full object-cover" />
-							</div>
-						</div>
-					</div>
+
 				</div>
 
 				<div class="w-1/2 border">
