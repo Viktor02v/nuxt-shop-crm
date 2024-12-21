9601153a92d5802f43e359a04694f82a19c40237
Viktor Petruk <damnnn261@gmail.com>
2024-12-21 20:59:09 +0200

    create (Overview.vue, Form.vue) components

--- pages/creation.vue
+++ pages/creation.vue
@@ -18,30 +18,19 @@ const formValues = ref({
 
 		<div class="mt-10">
 			<div class="border p-5 bg-gradient-to-r from-[#020817] to-[#0F172A]">
-				<div class="flex justify-center mb-5">
-					<h2 class="text-[1.3rem] p-2 bg-gradient-to-b from-[#020817] to-[#0F172A] border rounded">
-						Create item
-					</h2>
-				</div>
-				<div class="flex flex-col gap-10 items-center justify-center ">
-					<div class="w-1/2 border-b pb-10">
+
+				<div class="flex gap-10 items-start justify-center ">
+
+					<div class="w-1/2 border p-5 bg-gradient-to-b from-[#020817] to-[#0F172A]">
+						<div class="flex justify-center mb-5">
+							<h2 class="text-[1.3rem] p-2 bg-gradient-to-b from-[#020817] to-[#0F172A] border rounded">
+								Create item
+							</h2>
+						</div>
 						<LayoutCreationForm v-model:form-values="formValues" />
 					</div>
 
-					<div class="w-1/2">
-						<div class="flex justify-center mb-5">
-							<h2 class="text-[1.3rem] p-2 bg-gradient-to-b from-[#020817] to-[#0F172A] border rounded">
-								Overview
-							</h2>
-						</div>
-
-						<div class="w-full ">
-							<div class="flex justify-center">
-								<div class="flex flex-col">
-								</div>
-							</div>
-						</div>
-					</div>
+					<LazyLayoutCreationOverview :form-values="formValues" />
 				</div>
 			</div>
 		</div>
@@ -49,4 +38,19 @@ const formValues = ref({
 </template>
 
 <style scoped>
+@keyframes show {
+	from {
+		transform: scale(0.5) translateY(-30px);
+		opacity: 0.4;
+	}
+
+	to {
+		transform: scale(1) translateY(0);
+		opacity: 1;
+	}
+}
+
+.animation {
+	animation: show 0.6s ease-in-out;
+}
 </style>
